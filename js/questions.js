292ada6d29dd7947761e910c425d9c7eb106a3ba
Stephan Brisard <stephanbrisard123@gmail.com>
2021-04-14 19:12:31 -0400

    Assignment 4 Project
    Completed Quiz
    Added Javascript

---
+++ js/questions.js
@@ -0,0 +1,224 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following tag is used to mark the beginning of paragraph?",
+        answer: "P",
+        options: [
+            "TD",
+            "br",
+            "P",
+            "TR"
+        ]
+    },
+    {
+        numb: 3,
+        question: "From which tag descriptive list starts?",
+        answer: "DL",
+        options: [
+            "LL",
+            "DD",
+            "DL",
+            "DS"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Correct HTML Tag for the largest heading is?",
+        answer: "h1",
+        options: [
+            "head",
+            "h6",
+            "heading",
+            "h1"
+        ]
+    },
+    {
+        numb: 5,
+        question: "The attribute of form tag?",
+        answer: "Both A and B",
+        options: [
+            "Method",
+            "Action",
+            "Both A and B",
+            "None of these"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Markup tags tell the web browser?",
+        answer: "How to display the page",
+        options: [
+            "How to organise the page",
+            "How to display the page",
+            "How to display message box on page",
+            "None of these"
+        ]
+    },
+
+    {
+        numb: 7,
+        question: "www is based on which model?",
+        answer: "Client-server",
+        options: [
+            "Local-server",
+            "Client-server",
+            "3-tier",
+            "None of these"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What are Empty elements and is it valid?",
+        answer: "Empty elements are element with no data",
+        options: [
+            "No, there is no such terms as Empty Element",
+            "Empty elements are element with no data",
+            "No, it is not valid to use Empty Element",
+            "None of these"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of the following attributes of text box control allow to limit the maximum chartacter?",
+        answer: "maxlength",
+        options: [
+            "size",
+            "len",
+            "maxlength",
+            "all of these"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Web pages starts with which of the follow tag?",
+        answer: "HTML",
+        options: [
+            "Body",
+            "Title",
+            "HTML",
+            "Form"
+        ]
+    },
+    {
+        numb: 11,
+        question: "HTML is a subset of",
+        answer: "SGML",
+        options: [
+            "SGMT",
+            "SGML",
+            "SGMD",
+            "None of these"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Which of the following is a container?",
+        answer: "Both a and b",
+        options: [
+            "SELECT",
+            "BODY",
+            "INPUT",
+            "Both a and b"
+        ]
+    },
+    {
+        numb: 13,
+        question: "The attribute, which define the relationship between current document and HREF'ed URL is",
+        answer: "REL",
+        options: [
+            "REL",
+            "URL",
+            "REV",
+            "all of these"
+        ]
+    },
+    {
+        numb: 14,
+        question: "DT tag is desginer to fit a single line of our web page but DD tag will accept a",
+        answer: "full paragraph",
+        options: [
+            "line of text",
+            "full paragraph",
+            "word",
+            "request"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Character encoding is",
+        answer: "a system that consists of a code which pairs each character with a pattern, sequence of natural numbers or electrical pulse in order to transmit the data",
+        options: [
+            "method used to represent numbers in a character",
+            "method used to represent character in a number",
+            "a system that consists of a code which pairs each character with a pattern, sequence of natural numbers or electrical pulse in order to transmit the data",
+            "none of these"
+        ]
+    },
+    {
+        numb: 16,
+        question: "From which tag the descriptive list starts",
+        answer: "DL",
+        options: [
+            "LL",
+            "DD",
+            "DL",
+            "DS"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Correct HTML to left align the content inside a table cell is",
+        answer: "td align = left",
+        options: [
+            "td left",
+            "td raligh = left",
+            "td align = left",
+            "td leftalign"
+        ]
+    },
+    {
+        numb: 18,
+        question: "The tag which allows you to rest other HTML tags within the description is",
+        answer: "CAPTION",
+        options: [
+            "TH",
+            "TD",
+            "TR",
+            "CAPTION"
+        ]
+    },
+    {
+        numb: 19,
+        question: "BASE tag is designed to appear only between",
+        answer: "HEAD",
+        options: [
+            "HEAD",
+            "TITLE",
+            "BODY",
+            "FORM"
+        ]
+    },
+    {
+        numb: 20,
+        question: "How can you open a link in a new browser window?",
+        answer: "a href = url target = _blank",
+        options: [
+            "a href = url target = new",
+            "a href = url target _blank",
+            "a href = url .new",
+            "a href = url target = open"
+        ]
+    },
+];
